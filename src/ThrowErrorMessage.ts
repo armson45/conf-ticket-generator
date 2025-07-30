8916089b2e4031e72bb4801ab28d9a0cddaffe9f
armson45 <davds.armson45@gmail.com>
2025-07-29 21:00:28 -0600

    feat: Implement avatar upload functionality with validation and error handling

---
+++ src/ThrowErrorMessage.ts
@@ -0,0 +1,6 @@
+export function throwErrorMessage(message: string, element: string) {
+  const errorMessageContainer = document.createElement('div');
+
+  errorMessageContainer.classList.add('error');
+  // continuar
+}
--- src/pages/api/generate-ticket.js
+++ src/pages/api/generate-ticket.js
@@ -1,12 +1,27 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 export async function POST({ request, redirect }) {
   const formData = await request.formData();
+
+  const avatar = formData.get('avatar');
   const fullName = formData.get('fullname');
   const email = formData.get('email');
   const githubUser = formData.get('githubuser');
 
+  const nameAvatar = `${Date.now()}-${avatar.name}`;
+
+  const pathToImage = path.join("public", "uploads", nameAvatar);
+  const buffer = Buffer.from(await avatar.arrayBuffer());
+  await fs.writeFile(pathToImage, buffer);
+  const imageUrl = `/uploads/${nameAvatar}`
+
   const params = new URLSearchParams();
+  params.append('avatar', imageUrl);
   params.append('fullname', fullName);
   params.append('email', email);
   params.append('githubuser', githubUser);
+
+
   return redirect(`/ticket?${params.toString()}`);
 }
