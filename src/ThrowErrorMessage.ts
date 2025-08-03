4757fc8876da3f348af170cf00aa89a60f2d4a9f
armson45 <davds.armson45@gmail.com>
2025-08-02 19:09:15 -0600

    added: avatar photo validations with error messages in all fields

--- src/ThrowErrorMessage.ts
+++
@@ -1,6 +0,0 @@
-export function throwErrorMessage(message: string, element: string) {
-  const errorMessageContainer = document.createElement('div');
-
-  errorMessageContainer.classList.add('error');
-  // continuar
-}
